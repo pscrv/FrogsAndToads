594d81f1780ab97bd9bdc082d5bd59f7eb26fd24
pscrv <scrv.pscrv@googlemail.com>
2018-01-30 23:42:21 +0100

    More tidy still

--- FrogsAndToadsCore/FrogsAndToadsPositionEvaluator.cs
+++ FrogsAndToadsCore/FrogsAndToadsPositionEvaluator.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+
 using GameCore;
 using Monads;
 
@@ -23,7 +23,6 @@ namespace FrogsAndToadsCore
             return -EvaluateEndPositionForLeft(position.Reverse());
         }
         #endregion
-
 
 
         #region internal methods
